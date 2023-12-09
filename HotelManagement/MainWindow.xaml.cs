687f89e92fc6d657e4e5ac7e834fe56a9baeef91
22520650 <22520650@gm.uit.edu.vn>
2023-12-09 18:39:56 +0700

    setting

--- HotelManagement/MainWindow.xaml.cs
+++ HotelManagement/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace HotelManagement;
 
@@ -19,5 +10,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void btnClose_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
