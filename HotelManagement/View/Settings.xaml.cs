00cac84cddf6b7bc572f2f5b76b2e582117b3182
22520650 <22520650@gm.uit.edu.vn>
2023-12-10 17:41:35 +0700

    suaaddsetting 10/12

--- HotelManagement/View/Settings.xaml.cs
+++ HotelManagement/View/Settings.xaml.cs
@@ -4,8 +4,24 @@ namespace HotelManagement.View;
 
 public partial class Settings : UserControl
 {
+
+
     public Settings()
     {
         InitializeComponent();
+        btnLight.IsChecked = true;
+        cbboxLanguage.SelectedIndex = 0;
     }
+
+    private void btnLight_Checked(object sender, System.Windows.RoutedEventArgs e)
+    {
+        btnDark.IsChecked = false;
+    }
+
+    private void btnDark_Checked(object sender, System.Windows.RoutedEventArgs e)
+    {
+        btnLight.IsChecked = false;
+    }
+
+
 }
