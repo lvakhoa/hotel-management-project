9304c4b3569aae25fb6d6006f4a25a6daa53f94a
lvakhoa <22520674@gm.uit.edu.vn>
2023-12-12 18:36:39 +0700

    Fix settings

--- HotelManagement/View/Settings.xaml.cs
+++ HotelManagement/View/Settings.xaml.cs
@@ -1,4 +1,7 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace HotelManagement.View;
 
@@ -7,5 +10,34 @@ public partial class Settings : UserControl
     public Settings()
     {
         InitializeComponent();
+        btnLight.IsChecked = true;
+    }
+
+    private void btnLight_Checked(object sender, System.Windows.RoutedEventArgs e)
+    {
+        btnDark.IsChecked = false;
+    }
+
+    private void btnDark_Checked(object sender, System.Windows.RoutedEventArgs e)
+    {
+        btnLight.IsChecked = false;
+    }
+
+    private void EditTb_OnMouseEnter(object sender, MouseEventArgs e)
+    {
+            TextDecoration myUnderline = new TextDecoration
+            {
+                Pen = new Pen(new BrushConverter().ConvertFrom("#373434") as SolidColorBrush, 1),
+                PenThicknessUnit = TextDecorationUnit.FontRecommended,
+                PenOffset = 3
+            };
+
+            TextDecorationCollection myCollection = new TextDecorationCollection { myUnderline };
+            EditTb.TextDecorations = myCollection;
+    }
+
+    private void EditTb_OnMouseLeave(object sender, MouseEventArgs e)
+    {
+        EditTb.TextDecorations = null;
     }
 }
