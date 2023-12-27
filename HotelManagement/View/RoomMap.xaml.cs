b7eb3015b3907b42457f453c941b5a8cdd2bca9c
22520650 <22520650@gm.uit.edu.vn>
2023-12-27 21:59:54 +0700

    fix chart

--- HotelManagement/View/RoomMap.xaml.cs
+++ HotelManagement/View/RoomMap.xaml.cs
@@ -1,22 +1,5 @@
-using HotelManagement.Model;
 using HotelManagement.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using CommunityToolkit.Mvvm.Messaging;
-using System.Net;
-using System.Collections.ObjectModel;
 
 namespace HotelManagement.View;
 
@@ -26,6 +9,7 @@ public partial class RoomMap : UserControl
     public RoomMap()
     {
         InitializeComponent();
+        _ = RoomVM.GetRoomList();
         ShowListRoom.SelectedIndex = 0;
     }
     private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
